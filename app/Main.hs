69796cf695959567c57e91c1b2686cd0805fe0b6
lesguillemets <lesguillemets@users.noreply.github.com>
2017-09-28 15:58:24 +0900

    integral and expectation

--- app/Main.hs
+++ app/Main.hs
@@ -9,6 +9,6 @@ main :: IO ()
 main = do
     print $ calc  (binom 10) (0.2) 3
     print $ (0.2^3 * 0.8^7) * (fromIntegral $ 10 `c` 3)
-    print $ expectVal (f $ Normal 0 10) (const 1) (-1000) 1000 0.01
+    print $ expectValD (f $ Normal 0 10) (const 1) (-1000) 1000 0.01
     where
         n `c` x = numerator $ product [(n-i) % (x-i) | i <- [0..x-1]]
