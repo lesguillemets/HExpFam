917b743894064d24cf9dec7d7a379eb6fa11fe08
lesguillemets <lesguillemets@users.noreply.github.com>
2017-10-02 13:36:08 +0900

    better test... hopefully

--- test/Data/ExponentialFamily/Distribution/NormalSpec.hs
+++ test/Data/ExponentialFamily/Distribution/NormalSpec.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE ScopedTypeVariables #-}
 module Data.ExponentialFamily.Distribution.NormalSpec (spec) where
 
 import Test.Hspec
@@ -16,48 +17,56 @@ import Data.ExponentialFamily.ThetaEta
 spec :: Spec
 spec = do
     describe "normal distribution" $ do
-        it "sums up to 1" $ property $ do
-            μ <- choose (-10, 10)
-            σ2 <- choose (0, 100)
-            return . (<0.001)  . abs $ 1 - expectVal (IntegrateDouble (-100) 100 0.01) (Normal μ σ2) (const 1)
-        it "logF is nicely defined" $ property $ do
-            n <- Normal <$> choose (-100, 100) <*>  choose (0.01, 10000)
+        it "sums up to 1" $ property $ \(norm::Normal) ->
+            (<0.001)  . abs $ 1 - expectVal (normConfig norm) norm (const 1)
+
+        it "logF is nicely defined" $ property $ \(n::Normal) -> do
             x <- choose (-200,200)
             return . (<0.001)  . abs $ logF n x - (log $ f n x)
             
-        it "fromθ . toθ is id" $ property $ do
-            par <- Normal <$> choose (-10, 10) <*> choose (0, 100)
-            return $ (fromθ . toθ $ par) `closeEnough` par
-        it "fromη . toη is id" $ property $ do
-            par <- Normal <$> choose (-10, 10) <*> choose (0, 100)
-            return $ (fromη . toη $ par) `closeEnough` par
-        it "modifyθ +k . -k is id" $ property $ do
-            par <- Normal <$> choose (-10, 10) <*> choose (0, 100)
-            k <- arbitrary
-            return $ (modifyθ (first $ subtract k) . modifyθ (first (+k))) par `closeEnough` par
+        it "fromθ . toθ is id" $ property $ \(par :: Normal) ->
+            (fromθ . toθ $ par) `closeEnough` par
+        it "fromη . toη is id" $ property $ \(par :: Normal) ->
+            (fromη . toη $ par) `closeEnough` par
+        it "modifyθ +k . -k is id" $ property $ \((par,k) :: (Normal, Double)) ->
+                (modifyθ (first $ subtract k) . modifyθ (first (+k))) par `closeEnough` par
                     &&
                     (modifyθ (second $ subtract k) . modifyθ (second (+k))) par `closeEnough` par
-        it "modifyη +k . -k is id" $ property $ do
-            par <- Normal <$> choose (-10, 10) <*> choose (0, 100)
-            k <- arbitrary
-            return $  (modifyη (first $ subtract k) . modifyη (first (+k))) par `closeEnough` par
+        it "modifyη +k . -k is id" $ property $ \((par,k) :: (Normal, Double)) ->
+                (modifyη (first $ subtract k) . modifyη (first (+k))) par `closeEnough` par
                     &&
                     (modifyη (second $ subtract k) . modifyη (second (+k))) par `closeEnough` par
-        it "looks ok" $ property $ do
-            x0 <- Normal <$> choose (-10, 10) <*> choose (0.1, 100)
+        it "looks ok" $ property $ \(x0 :: Normal) ->
             let x1 = modifyθ (first (+0.1)) x0
                 x2 = modifyη (second (+0.1)) x1
                 kld1 = kld x0 x2
                 kld2 = kld x0 x1 + kld x1 x2
-            return $ if abs (kld1 - kld2) < 0.1
-                        then succeeded
-                        else failed {
-                            QP.reason =
-                                (show (x0, x1, x2, kld1, kld2, abs (kld1-kld2)))
-                        }
+                in  abs (kld1 - kld2) < 0.1
 
 closeEnough :: Normal -> Normal -> Bool
 closeEnough (Normal a b) (Normal c d) =
     abs (a - c) < 0.0001 && abs (b - d) < 0.0001
 
-kld = kullbackLeiblerDivergence (IntegrateDouble (-100) 100 0.01)
+kld n0 n1 = kullbackLeiblerDivergence (norm2Config n0 n1) n0 n1
+
+instance Arbitrary Normal where
+    arbitrary = do
+        μ <- choose (-100,100)
+        σ2 <- choose (0.01, 10000)
+        return $ Normal μ σ2
+
+normConfig :: Normal -> IntegrateDouble
+normConfig (Normal μ σ2) =
+    let σ = sqrt σ2
+        in
+    IntegrateDouble (μ-4*σ) (μ+4*σ) (8*σ/10000)
+
+
+norm2Config :: Normal -> Normal -> IntegrateDouble
+norm2Config (Normal μ0 σ20) (Normal μ1 σ21)=
+    let σ0 = sqrt σ20
+        σ1 = sqrt σ21
+        lower = min (μ0-2*σ0) (μ1-2*σ1)
+        upper = max (μ0+2*σ0) (μ1+2*σ1)
+        in
+    IntegrateDouble  lower upper ((upper-lower)/10000)
