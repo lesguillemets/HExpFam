f57e28f6448a48b0c86180d37903deec61d3d472
lesguillemets <lesguillemets@users.noreply.github.com>
2017-10-02 15:42:40 +0900

    add basic tests for binomial theta-eta

--- test/Data/ExponentialFamily/Distribution/BinomialSpec.hs
+++ test/Data/ExponentialFamily/Distribution/BinomialSpec.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE ScopedTypeVariables #-}
 module Data.ExponentialFamily.Distribution.BinomialSpec (spec) where
 
 import Test.Hspec
@@ -5,6 +6,7 @@ import Test.QuickCheck
 import Test.QuickCheck.Property as QP
 import Test.QuickCheck.Arbitrary
 
+import Control.Arrow
 import Data.ExponentialFamily.Distribution.Binomial
 import Data.ExponentialFamily.Integration
 import Data.ExponentialFamily.Density
@@ -18,4 +20,23 @@ spec = do
             n <- choose (1,15)
             p <- choose (0,1)
             return . (<0.001)  . abs $ 1 - expectVal [0..n] (Binomial n p) (const 1)
+        it "fromθ . toθ is id" $ property $ \(bin :: Binomial) ->
+            (fromθ . toθ $ bin) `closeEnough` bin
+        it "fromη . toη is id" $ property $ \(bin :: Binomial) ->
+            (fromη . toη $ bin) `closeEnough` bin
+        it "modifyθ +k . -k is id" $ property $ \((bin,Dk k) :: (Binomial, Dk)) ->
+                (modifyθ (first $ subtract k) . modifyθ (first (+k))) bin `closeEnough` bin
 
+closeEnough :: Binomial -> Binomial -> Bool
+closeEnough (Binomial a b) (Binomial c d) =
+    a == c && abs (b - d) < 0.0001
+
+instance Arbitrary Binomial where
+    arbitrary = Binomial <$> choose (1,15) <*> choose (0,1)
+
+
+newtype Dk = Dk Double
+instance Show Dk where
+    show (Dk k) = show k
+instance Arbitrary Dk where
+    arbitrary = Dk <$> choose (-0.02,0.02)
