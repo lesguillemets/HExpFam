f90b4b6a31b03ee04cad83d9fc19a4fadc4aec3f
lesguillemets <lesguillemets@users.noreply.github.com>
2017-10-02 15:19:34 +0900

    TEParam instance for Binomial

--- src/Data/ExponentialFamily/Distribution/Binomial.hs
+++ src/Data/ExponentialFamily/Distribution/Binomial.hs
@@ -16,3 +16,12 @@ instance ProbDensity Binomial where
     type Point Binomial = Int
     f (Binomial n p) success =
         (n `choose` success) * p^success * (1-p)^(n-success)
+
+-- | (η,N) and (θ,N) for 'known' number of trials : N.
+type instance TEParam Binomial = (Double, Int)
+instance ThetaEta Binomial where
+    toθ (Binomial n p)  = Theta (log p / (1-p), n)
+    fromθ (Theta (logit, n))
+        = Binomial n (exp logit / (1+exp logit))
+    toη (Binomial n p)  = Eta (fromIntegral n*p, n)
+    fromη (Eta (np, n)) = Binomial n (np/fromIntegral n)
